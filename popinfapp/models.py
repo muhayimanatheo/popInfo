e0ab5f428580500cdfc25c197827ae7ec783a1a7
muhayimanatheo <muhayitheo23@gmail.com>
2024-01-24 23:24:00 +0200

    page finished

--- popinfapp/models.py
+++ popinfapp/models.py
@@ -4,8 +4,12 @@ from django.db import models
 class Information(models.Model):
     fullname = models.CharField(max_length=200)
     birthdate = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField(max_length=200)
+    phone = models.CharField(max_length=200)
     age = models.IntegerField(default=None)
     gender = models.CharField(max_length=200)
     provence = models.CharField(max_length=200)
     familymembers = models.IntegerField(default=None)
+    resident = models.CharField(max_length=200)
+    bankaccount = models.IntegerField(default=None)
     
