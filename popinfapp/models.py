3835c559ea55e25b08e23da767ee709fd9f35379
muhayimanatheo <muhayitheo23@gmail.com>
2024-01-24 16:07:50 +0200

    start my html pages

--- popinfapp/models.py
+++ popinfapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Information(models.Model):
+    fullname = models.CharField(max_length=200)
+    birthdate = models.DateTimeField(auto_now_add=True)
+    age = models.IntegerField(default=None)
+    gender = models.CharField(max_length=200)
+    provence = models.CharField(max_length=200)
+    familymembers = models.IntegerField(default=None)
+    
