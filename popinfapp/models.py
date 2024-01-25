9447cb5ff410222300300159df465ef92ee563e6
muhayimanatheo <muhayitheo23@gmail.com>
2024-01-25 09:47:45 +0200

    insert successfully

--- popinfapp/models.py
+++ popinfapp/models.py
@@ -3,13 +3,9 @@ from django.db import models
 # Create your models here.
 class Information(models.Model):
     fullname = models.CharField(max_length=200)
-    birthdate = models.DateTimeField(auto_now_add=True)
-    email = models.EmailField(max_length=200)
     phone = models.CharField(max_length=200)
     age = models.IntegerField(default=None)
     gender = models.CharField(max_length=200)
     provence = models.CharField(max_length=200)
     familymembers = models.IntegerField(default=None)
-    resident = models.CharField(max_length=200)
-    bankaccount = models.IntegerField(null=False)
     
--- popinfapp/views.py
+++ popinfapp/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+   if request.method == 'POST':
+      pop= Information()
+      pop.fullname = request.POST['fullname']
+      pop.phone = request.POST['phone']
+      pop.age = request.POST['age']
+      pop.gender = request.POST['gender']
+      pop.provence = request.POST['provence']
+      pop.familymembers = request.POST['familymembers']
+      pop.save()
+   return render(request, 'index.html')
